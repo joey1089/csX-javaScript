e497ec67a3148c46bda414f10734f21ee16de26a
joey1089 <josephvpeter7@gmail.com>
2022-07-15 13:21:43 -0400

    added forloop.js

---
+++ forloop.js
@@ -0,0 +1,9 @@
+let countDown = 10;
+// ADD CODE HERE
+
+for(countDown; countDown > 0 ; countDown--){
+  //console.log(countDown);
+}
+// Uncomment the below line to check your work
+console.log(countDown); // -> should print 0;
+
