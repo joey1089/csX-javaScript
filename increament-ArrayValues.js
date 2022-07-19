de9c4b48e39cbb475d49759da154d8892ef9bf60
joey1089 <josephvpeter7@gmail.com>
2022-07-18 20:15:57 -0400

    added ; to increament-ArrayValues.js

--- increament-ArrayValues.js
+++ increament-ArrayValues.js
@@ -1,7 +1,7 @@
 const increaseByTwo = [1, 2, 3, 4, 5];
 // ADD CODE HERE
 for(let i=0;i<increaseByTwo.length;i++){
-  increaseByTwo[i] += 2
+  increaseByTwo[i] += 2;
 }
 
 // Uncomment the line below to check your work!
