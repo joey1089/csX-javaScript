df7e044dacd5b92b77b400f8f04f49c94c87356b
joey1089 <josephvpeter7@gmail.com>
2022-07-18 20:28:54 -0400

    added whileLoop.js

---
+++ whileLoop.js
@@ -0,0 +1,11 @@
+let count = 2;
+// ADD CODE HERE
+while(count < 8){
+  count += 2;
+}
+
+// Uncomment these to check your work!
+// A while loop is an alternative iteration statement. JavaScript's thread of execution 
+// will repeatedly process the code block until the while loop's conditional expression 
+// evaluates to false. **Be careful to avoid an infinite loop!**
+console.log(count); 
